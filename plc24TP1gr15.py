8c020b06a530515cf1a0a8ea212996f3673532c7
Johnny <fonsecajanjao@gmail.com>
2024-10-11 10:20:07 +0100

    Renomeado e com novos comentários

--- plc24TP1gr15.py
+++ plc24TP1gr15.py
@@ -4,5 +4,14 @@ Ex 2? 3?
 Acho q o ex5 vai ser meio complicado...
 '''
 
+#Imports
 import re
 import sys
+
+#Ex 3: r"([0-9][1-9]),(M|F),([0-9]{1,3}),([0-9]{1,3}),([0-9]{2,3}),(0|1)" (???)
+myHeart = open("myheart.csv", "r")
+myHeart.readline()
+linha = myHeart.readline()
+print(linha)
+''''''
+myHeart.close()
