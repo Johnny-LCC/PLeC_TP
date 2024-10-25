03ccf19826ae2c440b5b809af6849a7bd570adb8
Johnny <fonsecajanjao@gmail.com>
2024-10-25 17:46:49 +0100

    Ficheiros Atualizados

--- plc24TP1gr15.py
+++ plc24TP1gr15.py
@@ -1,5 +1,5 @@
 '''
-Resolução do exercício 3 pelo Grupo 15
+Resolução do exercício 2 pelo Grupo 15
 João Fonseca - A102512
 Alexis Correia - A102495
 '''
@@ -8,127 +8,114 @@ import re
 import matplotlib.pyplot as plt
 
 #Variáveis
-num_linhas = 0
-doentes, masc, fem = 0, 0, 0
-idades = {}
-col = []
-tensao, bpm = {}, {}
+num_linha = 0
+minIdade, maxIdade = 1000, 0
+masc, fem = 0, 0
+modsAno = {}
+apts, total = {}, {}
+nomes = []
+
+er1 = ",(\d+),([MF]),"
+er2 = ",(\d{4})-\d{2}-\d{2},.+,[MF],[A-z]+,([A-zãçé]+),"
+er3 = ".+,(\d{4})-\d{2}-\d{2},.+,(true|false)$"
+er4 = "([A-Z][A-z]+),([A-Z][A-z]+),\d+,M"
 
 #Leitura ficheiro
-myHeart = open("myheart.csv", "r")
-myHeart.readline()
-for linha in myHeart:
-    num_linhas += 1
-    res = re.match(r'(\d+),([MF]),(\d+),(\d+),(\d+),1$', linha) #ER
-    if  res:
-        doentes += 1
-        
-        i = int(res.group(1))
-        e = (i//5)-6
-        if e not in idades.keys():
-            idades[e] = 1
-        else:
-            idades[e] += 1
-        
-        if res.group(2) == "M":
+emd = open("emd.csv", "r")
+emd.readline()
+for linha in emd:
+    num_linha += 1
+    
+    res1 = re.search(rf'{er1}',linha)
+    if res1:
+        idade = int(res1.group(1))
+        if idade < minIdade:
+            minIdade = idade
+        if idade > maxIdade:
+            maxIdade = idade
+        if res1.group(2) == "M":
             masc += 1
-        else: 
+        else:
             fem += 1
+    
+    res2 = re.search(rf'{er2}', linha)
+    if res2:
+        ano = int(res2.group(1))
+        mod = res2.group(2)
+        if ano not in modsAno.keys():
+            modsAno[ano] = {}
+        if mod not in modsAno[ano].keys():
+            modsAno[ano][mod] = 0
+        modsAno[ano][mod] += 1
 
-        col.append(int(res.group(4)))
+    res3 = re.match(rf'{er3}', linha)
+    if res3:
+        ano = int(res3.group(1))
+        if ano not in apts.keys():
+            apts[ano] = 0
+        if ano not in total.keys():
+            total[ano] = 0
+        total[ano] += 1
+        if res3.group(2) == 'true':
+            apts[ano] += 1
 
-        t, b = int(res.group(3)), int(res.group(5))
-        if t not in tensao.keys():
-            tensao[t] = 1
-        else:
-            tensao[t] += 1
-        if b not in bpm.keys():
-            bpm[b] = 1
-        else:
-            bpm[b] += 1
-myHeart.close()
+    res4 = re.search(rf'{er4}', linha)
+    if res4:
+        nomes.append((res4.group(1),res4.group(2)))
+emd.close()
 
-#Cálculos auxiliares
-p_doentes = (doentes/num_linhas)*100
-p_masc = (masc/doentes)*100
-p_fem = (fem/doentes)*100
+#Cálculos auxiliares e Matplotlib
+c = ["#0A4BBD","gray"] # #0691D6
 
-xidades, yidades = [],[]
-for x,y in sorted(idades.items()):
-    i = (x+6)*5
-    f = i+4
-    xidades.append(f"{i}-{f}")
-    yidades.append(y)
+pMasc = (masc/num_linha)*100
+pFem = (fem/num_linha)*100
 
-dCol = {}
-for c in col:
-    if c>0:
-        c = (c//10)*10
-    if c not in dCol.keys():
-        dCol[c] = 1
-    else:
-        dCol[c] += 1
-xcol, ycol = [], []
-for x,y in sorted(dCol.items()):
-    if c != 0:
-        xcol.append(f"{x}-{x+9}")
-    else:
-        xcol.append("ND")
-    ycol.append(y)
-
-xtensao, ytensao = [], []
-for x,y in sorted(tensao.items()):
-    xtensao.append(x)
-    ytensao.append(y)
-
-xbpm, ybpm = [], []
-for x,y in sorted(bpm.items()):
-    xbpm.append(x)
-    ybpm.append(y)
-
-#Matplotlib
-c = ["#0A4BBD","#0691D6"]
-
-plt.pie([doentes,num_linhas-doentes], colors=c, startangle=90)
-plt.legend(["Doentes", "Saudáveis"], title="Pacientes: ")
-plt.savefig("imagem.png")
-plt.close()
-
-plt.pie([masc, fem], colors=c, startangle=90)
-plt.legend(["Masculino", "Feminino"], title = "Doentes por género: ")
+plt.pie([masc,fem], startangle=90)
+plt.legend(["Homens","Mulheres"])
 plt.savefig("imagem1.png")
 plt.close()
 
-plt.bar(xidades,yidades)
-plt.ylabel("Número doentes")
-plt.xlabel("Escalões Etários")
-plt.savefig("imagem2.png")
-plt.close()
-
+mods = {}
+anos = []
+for a in sorted(modsAno.keys()):
+    x, y = [], []
+    for m in sorted(modsAno[a].keys()):
+        x.append(m)
+        y.append(modsAno[a][m])
+        if m not in mods.keys():
+            mods[m] = 0
+        mods[m]+=modsAno[a][m]
+    plt.figure(figsize=(10,8))
+    plt.barh(x[::-1],y[::-1])
+    plt.title(f"{a}")
+    plt.savefig(f"imagem2-{a}.png")
+    plt.close()
+    anos.append(a)
+xmod = [i[0] for i in sorted(mods.items())]
+ymod = [i[1] for i in sorted(mods.items())]
 plt.figure(figsize=(10,8))
-plt.barh(xcol,ycol)
-plt.ylabel("Nível colesterol")
-plt.xlabel("Número doentes")
-plt.savefig("imagem3.png")
+plt.barh(xmod[::-1],ymod[::-1])
+plt.title("Total")
+plt.savefig("imagem2-total.png")
 plt.close()
 
-plt.plot(xtensao,ytensao)
-plt.title("Tensão")
-plt.savefig("imagem4.png")
-plt.close()
-
-plt.plot(xbpm, ybpm)
-plt.title("Batimentos")
-plt.savefig("imagem5.png")
-plt.close()
+for k in sorted(apts.keys()):
+    apt = apts[k]
+    rest = total[k] - apt
+    plt.pie([apt,rest], colors=c, startangle=90, explode=[0.05,0.05])
+    plt.legend(["Aptos", "Não aptos"])
+    plt.title(f"Aptos - {k}")
+    plt.savefig(f"imagem3-{k}.png")
+    plt.close()
 
 #Ficheiro HTML
+anos.sort()
 conteudo_html = f"""<!DOCTYPE html>
 <html lang="eng">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Ex3</title>
+    <title>Ex2</title>
     <style>
         body {{
             font-family: Arial, sans-serif;
@@ -141,7 +128,7 @@ conteudo_html = f"""<!DOCTYPE html>
             color: #333;
         }}
         h2 {{
-            color: #007BFF;
+            color: #0A4BBD;
         }}
         .section {{
             margin-bottom: 30px;
@@ -150,59 +137,93 @@ conteudo_html = f"""<!DOCTYPE html>
             border-radius: 8px;
             box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
         }}
-        .img-container {{
-            text-align: center;
-        }}
         img {{
-            text-align: center;
             max-width: 100%;
             height: auto;
             border-radius: 5px;
         }}
+        .main-image {{
+            width: 500px;
+            height: auto;
+        }}
+        .thumbnail {{
+            width: 100px;
+            height: auto;
+            cursor: pointer;
+            margin: 10px;
+        }}
+        .thumbnail-container {{
+            text-align: center;
+        }}
+        .img-container{{
+            text-align: center;
+            margin-bottom: 20px;
+        }}
     </style>
 </head>
 <body>
-    <h1>Processador de registos de Doenças Cardíacas</h1>
+    <h1>Exames Médicos Desportivos</h1>
     <div class="section">
-        <h2>Doentes Totais e por Género</h2>
-        <p>Dentre os {num_linhas} pacientes, {doentes} encontram-se doentes. Isso representa {p_doentes:.2f}% do total de pacientes.</p>
-        <div class="img-container">
-            <img src="imagem.png" alt="Porcentagem doentes">
-        </div>
-        <p>Dos {doentes} pacientes doentes, {masc} são homens e {fem} são mulheres. Isso representa, respetivamente, {p_masc:.2f}% e {p_fem:.2f}% dos pacientes doentes.</p>
-        <div class="img-container">
-            <img src="imagem1.png" alt="Porcentagem homem/mulher">
-        </div>
+        <h2>Idades Extremas do Dataset</h2>
+        <p>Neste dataset, a idade mínima é {minIdade} e a idade máxima é {maxIdade}.</p>
     </div>
     <div class="section">
-        <h2>Distribuição por Escalões Etários</h2>
-        <p>Abaixo podemos ver o gráfico em barras que representa a distribuição dos doentes de acordo com seu Escalão de Idade.</p>
+        <h2>Distribuição dos atletas por género</h2>
+        <p>Dentre os {num_linha} atletas, {masc} são homens e {fem} são mulheres.</p>
         <div class="img-container">
-            <img src="imagem2.png" alt="Barras idades doentes">
+            <img src="imagem1.png" alt="Pie Chart">
         </div>
+        <p>Isso representa {pMasc:.2f}% e {pFem:.2f}% respetivamente</p>
     </div>
     <div class="section">
-        <h2>Distribuição por Níveis de Colesterol</h2>
-        <p>Esta é a distribuição de doentes de acordo com os seus níveis de colesterol. Atente-se que "ND" significa "No Data" e representa o número de pacientes cujos níveis de colesterol são desconhecidos.</p>
+        <h2>Distribuição das modalidades desportivas</h2>
+        <p>Texto.</p>
         <div class="img-container">
-            <img src="imagem3.png" alt="Barras colesterol doentes">
+            <img id="imagemPrincipal1" class="main-image" src="imagem2-total.png" alt="Imagem Principal">
+        </div>
+        <div class="thumbnail-container">"""
+for a in anos:
+    conteudo_html = conteudo_html + f"""
+            <img class="thumbnail" src="imagem2-{a}.png" alt="Modalidades {a}" onclick="trocarImagem('imagem2-{a}.png')">"""
+conteudo_html = conteudo_html +  f"""
+            <img class="thumbnail" src="imagem2-total.png" alt="Modalidades" onclick="trocarImagem('imagem2-total.png')">
         </div>
     </div>
+    <script>
+        function trocarImagem(src) {{
+            document.getElementById('imagemPrincipal1').src = src;
+        }}
+    </script>
     <div class="section">
-        <h2>Correlação Tensão/Batimento e Doença</h2>
-        <p>Com auxílio de ambos os gráficos abaixo podemos ver a correlação entre a Tensão e Batimentos com a doença.</p>
-        <p>Primeiro, veremos o gráfico que mostra a tensão (eixo x) e o número de paciente doentes correspondentes (eixo y).</p>
+        <h2>Aptos</h2>
+        <p>Texto.</p>
         <div class="img-container">
-            <img src="imagem4.png" alt="Tensão">
+            <img id="imagemPrincipal2" class="main-image" src="imagem3-{anos[0]}.png" alt="Imagem Principal">
         </div>
-        <p>Em seguido, veremos a mesma coisa com os Batimentos dos pacientes.</p>
-        <div class="img-container">
-            <img src="imagem5.png" alt="Batimentos">
+        <div class="thumbnail-container">"""
+for a in anos:
+    conteudo_html = conteudo_html + f"""
+            <img class="thumbnail" src="imagem3-{a}.png" alt="Aptos - {a}" onclick="trocarImagem2('imagem3-{a}.png')">"""
+conteudo_html = conteudo_html +  """
         </div>
     </div>
+    <script>
+        function trocarImagem2(src) {{
+            document.getElementById('imagemPrincipal2').src = src;
+        }}
+    </script>
 </body>
-</html>
-"""
+</html>"""
 ficheiro = open("index.html", "w", encoding="utf-8")
 ficheiro.write(conteudo_html)
+ficheiro.close()
+
+json = """["""
+for i in range(len(nomes)):
+    json = json + f"""\n    {{"apelido":"{nomes[i][0]}", "nome":"{nomes[i][1]}"}}"""
+    if i < len(nomes)-1:
+        json = json + ","
+json = json + """\n]"""
+ficheiro = open("nomes.json", "w", encoding="utf-8")
+ficheiro.write(json)
 ficheiro.close()
--- plc24TP1gr15_2.py
+++ plc24TP1gr15_2.py
@@ -1,5 +1,5 @@
 '''
-Resolução do exercício 2 pelo Grupo 15
+Resolução do exercício 3 pelo Grupo 15
 João Fonseca - A102512
 Alexis Correia - A102495
 '''
@@ -8,114 +8,125 @@ import re
 import matplotlib.pyplot as plt
 
 #Variáveis
-num_linha = 0
-minIdade, maxIdade = 1000, 0
-masc, fem = 0, 0
-modsAno = {}
-apts, total = {}, {}
-nomes = []
-
-er1 = ",(\d+),([MF]),"
-er2 = ",(\d{4})-\d{2}-\d{2},.+,[MF],[A-z]+,([A-zãçé]+),"
-er3 = ".+,(\d{4})-\d{2}-\d{2},.+,(true|false)$"
-er4 = "([A-Z][A-z]+),([A-Z][A-z]+),\d+,M"
+num_linhas = 0
+doentes, masc, fem = 0, 0, 0
+idades = {}
+col = []
+tensao, bpm = {}, {}
 
 #Leitura ficheiro
-emd = open("emd.csv", "r")
-emd.readline()
-for linha in emd:
-    num_linha += 1
-    
-    res1 = re.search(rf'{er1}',linha)
-    if res1:
-        idade = int(res1.group(1))
-        if idade < minIdade:
-            minIdade = idade
-        if idade > maxIdade:
-            maxIdade = idade
-        if res1.group(2) == "M":
-            masc += 1
+myHeart = open("myheart.csv", "r")
+myHeart.readline()
+for linha in myHeart:
+    num_linhas += 1
+    res = re.match(r'(\d+),([MF]),(\d+),(\d+),(\d+),1$', linha) #ER
+    if  res:
+        doentes += 1
+        
+        i = int(res.group(1))
+        e = (i//5)-6
+        if e not in idades.keys():
+            idades[e] = 1
         else:
+            idades[e] += 1
+        
+        if res.group(2) == "M":
+            masc += 1
+        else: 
             fem += 1
-    
-    res2 = re.search(rf'{er2}', linha)
-    if res2:
-        ano = int(res2.group(1))
-        mod = res2.group(2)
-        if ano not in modsAno.keys():
-            modsAno[ano] = {}
-        if mod not in modsAno[ano].keys():
-            modsAno[ano][mod] = 0
-        modsAno[ano][mod] += 1
 
-    res3 = re.match(rf'{er3}', linha)
-    if res3:
-        ano = int(res3.group(1))
-        if ano not in apts.keys():
-            apts[ano] = 0
-        if ano not in total.keys():
-            total[ano] = 0
-        total[ano] += 1
-        if res3.group(2) == 'true':
-            apts[ano] += 1
+        col.append(int(res.group(4)))
 
-    res4 = re.search(rf'{er4}', linha)
-    if res4:
-        nomes.append((res4.group(1),res4.group(2)))
-emd.close()
+        t, b = int(res.group(3)), int(res.group(5))
+        if t not in tensao.keys():
+            tensao[t] = 1
+        else:
+            tensao[t] += 1
+        if b not in bpm.keys():
+            bpm[b] = 1
+        else:
+            bpm[b] += 1
+myHeart.close()
 
-#Cálculos auxiliares e Matplotlib
-c = ["#0A4BBD","#0691D6"]
+#Cálculos auxiliares
+p_doentes = (doentes/num_linhas)*100
+p_masc = (masc/doentes)*100
+p_fem = (fem/doentes)*100
 
-pMasc = (masc/num_linha)*100
-pFem = (fem/num_linha)*100
+xidades, yidades = [],[]
+for x,y in sorted(idades.items()):
+    i = (x+6)*5
+    f = i+4
+    xidades.append(f"{i}-{f}")
+    yidades.append(y)
 
-plt.pie([masc,fem], colors=c, startangle=90)
-plt.legend(["Homens","Mulheres"])
+dCol = {}
+for c in col:
+    if c>0:
+        c = (c//10)*10
+    if c not in dCol.keys():
+        dCol[c] = 1
+    else:
+        dCol[c] += 1
+xcol, ycol = [], []
+for x,y in sorted(dCol.items()):
+    if c != 0:
+        xcol.append(f"{x}-{x+9}")
+    else:
+        xcol.append("ND")
+    ycol.append(y)
+
+xtensao, ytensao = [], []
+for x,y in sorted(tensao.items()):
+    xtensao.append(x)
+    ytensao.append(y)
+
+xbpm, ybpm = [], []
+for x,y in sorted(bpm.items()):
+    xbpm.append(x)
+    ybpm.append(y)
+
+#Matplotlib
+plt.pie([doentes,num_linhas-doentes], startangle=90)
+plt.legend(["Doentes", "Saudáveis"], title="Pacientes: ")
+plt.savefig("imagem.png")
+plt.close()
+
+plt.pie([masc, fem], startangle=90)
+plt.legend(["Masculino", "Feminino"], title = "Doentes por género: ")
 plt.savefig("imagem1.png")
 plt.close()
 
-mods = {}
-anos = []
-for a in sorted(modsAno.keys()):
-    x, y = [], []
-    for m in sorted(modsAno[a].keys()):
-        x.append(m)
-        y.append(modsAno[a][m])
-        if m not in mods.keys():
-            mods[m] = 0
-        mods[m]+=modsAno[a][m]
-    plt.figure(figsize=(10,8))
-    plt.barh(x[::-1],y[::-1])
-    plt.title(f"{a}")
-    plt.savefig(f"imagem2-{a}.png")
-    plt.close()
-    anos.append(a)
-xmod = [i[0] for i in sorted(mods.items())]
-ymod = [i[1] for i in sorted(mods.items())]
-plt.figure(figsize=(10,8))
-plt.barh(xmod[::-1],ymod[::-1])
-plt.title("Total")
-plt.savefig("imagem2-total.png")
+plt.bar(xidades,yidades)
+plt.ylabel("Número doentes")
+plt.xlabel("Escalões Etários")
+plt.savefig("imagem2.png")
 plt.close()
 
-for k in sorted(apts.keys()):
-    apt = apts[k]
-    rest = total[k] - apt
-    plt.pie([apt,rest], colors=c, startangle=90)
-    plt.legend(["Aptos", "Não aptos"])
-    plt.title(f"Aptos - {k}")
-    plt.savefig(f"imagem3-{k}.png")
-    plt.close()
+plt.figure(figsize=(10,8))
+plt.barh(xcol,ycol)
+plt.ylabel("Nível colesterol")
+plt.xlabel("Número doentes")
+plt.savefig("imagem3.png")
+plt.close()
+
+plt.plot(xtensao,ytensao)
+plt.title("Tensão")
+plt.savefig("imagem4.png")
+plt.close()
+
+plt.plot(xbpm, ybpm)
+plt.title("Batimentos")
+plt.savefig("imagem5.png")
+plt.close()
 
 #Ficheiro HTML
-anos.sort()
 conteudo_html = f"""<!DOCTYPE html>
 <html lang="eng">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Ex2</title>
+    <title>Ex3</title>
     <style>
         body {{
             font-family: Arial, sans-serif;
@@ -128,7 +139,7 @@ conteudo_html = f"""<!DOCTYPE html>
             color: #333;
         }}
         h2 {{
-            color: #007BFF;
+            color: #0A4BBD;
         }}
         .section {{
             margin-bottom: 30px;
@@ -137,93 +148,59 @@ conteudo_html = f"""<!DOCTYPE html>
             border-radius: 8px;
             box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
         }}
+        .img-container {{
+            text-align: center;
+        }}
         img {{
+            text-align: center;
             max-width: 100%;
             height: auto;
             border-radius: 5px;
         }}
-        .main-image {{
-            width: 500px;
-            height: auto;
-        }}
-        .thumbnail {{
-            width: 100px;
-            height: auto;
-            cursor: pointer;
-            margin: 10px;
-        }}
-        .thumbnail-container {{
-            text-align: center;
-        }}
-        .img-container{{
-            text-align: center;
-            margin-bottom: 20px;
-        }}
     </style>
 </head>
 <body>
-    <h1>Exames Médicos Desportivos</h1>
+    <h1>Processador de registos de Doenças Cardíacas</h1>
     <div class="section">
-        <h2>Idades Extremas do Dataset</h2>
-        <p>Neste dataset, a idade mínima é {minIdade} e a idade máxima é {maxIdade}.</p>
-    </div>
-    <div class="section">
-        <h2>Distribuição dos atletas por género</h2>
-        <p>Dentre os {num_linha} atletas, {masc} são homens e {fem} são mulheres.</p>
+        <h2>Doentes Totais e por Género</h2>
+        <p>Dentre os {num_linhas} pacientes, {doentes} encontram-se doentes. Isso representa {p_doentes:.2f}% do total de pacientes.</p>
         <div class="img-container">
-            <img src="imagem1.png" alt="Pie Chart">
+            <img src="imagem.png" alt="Porcentagem doentes">
         </div>
-        <p>Isso representa {pMasc:.2f}% e {pFem:.2f}% respetivamente</p>
+        <p>Dos {doentes} pacientes doentes, {masc} são homens e {fem} são mulheres. Isso representa, respetivamente, {p_masc:.2f}% e {p_fem:.2f}% dos pacientes doentes.</p>
+        <div class="img-container">
+            <img src="imagem1.png" alt="Porcentagem homem/mulher">
+        </div>
     </div>
     <div class="section">
-        <h2>Distribuição das modalidades desportivas</h2>
-        <p>Texto.</p>
+        <h2>Distribuição por Escalões Etários</h2>
+        <p>Abaixo podemos ver o gráfico em barras que representa a distribuição dos doentes de acordo com seu Escalão de Idade.</p>
         <div class="img-container">
-            <img id="imagemPrincipal1" class="main-image" src="imagem2-total.png" alt="Imagem Principal">
-        </div>
-        <div class="thumbnail-container">"""
-for a in anos:
-    conteudo_html = conteudo_html + f"""
-            <img class="thumbnail" src="imagem2-{a}.png" alt="Modalidades {a}" onclick="trocarImagem('imagem2-{a}.png')">"""
-conteudo_html = conteudo_html +  f"""
-            <img class="thumbnail" src="imagem2-total.png" alt="Modalidades" onclick="trocarImagem('imagem2-total.png')">
+            <img src="imagem2.png" alt="Barras idades doentes">
         </div>
     </div>
-    <script>
-        function trocarImagem(src) {{
-            document.getElementById('imagemPrincipal1').src = src;
-        }}
-    </script>
     <div class="section">
-        <h2>Aptos</h2>
-        <p>Texto.</p>
+        <h2>Distribuição por Níveis de Colesterol</h2>
+        <p>Esta é a distribuição de doentes de acordo com os seus níveis de colesterol. Atente-se que "ND" significa "No Data" e representa o número de pacientes cujos níveis de colesterol são desconhecidos.</p>
         <div class="img-container">
-            <img id="imagemPrincipal2" class="main-image" src="imagem3-{anos[0]}.png" alt="Imagem Principal">
-        </div>
-        <div class="thumbnail-container">"""
-for a in anos:
-    conteudo_html = conteudo_html + f"""
-            <img class="thumbnail" src="imagem3-{a}.png" alt="Aptos - {a}" onclick="trocarImagem2('imagem3-{a}.png')">"""
-conteudo_html = conteudo_html +  """
+            <img src="imagem3.png" alt="Barras colesterol doentes">
         </div>
     </div>
-    <script>
-        function trocarImagem2(src) {{
-            document.getElementById('imagemPrincipal2').src = src;
-        }}
-    </script>
+    <div class="section">
+        <h2>Correlação Tensão/Batimento e Doença</h2>
+        <p>Com auxílio de ambos os gráficos abaixo podemos ver a correlação entre a Tensão e Batimentos com a doença.</p>
+        <p>Primeiro, veremos o gráfico que mostra a tensão (eixo x) e o número de paciente doentes correspondentes (eixo y).</p>
+        <div class="img-container">
+            <img src="imagem4.png" alt="Tensão">
+        </div>
+        <p>Em seguido, veremos a mesma coisa com os Batimentos dos pacientes.</p>
+        <div class="img-container">
+            <img src="imagem5.png" alt="Batimentos">
+        </div>
+    </div>
 </body>
-</html>"""
+</html>
+"""
 ficheiro = open("index.html", "w", encoding="utf-8")
 ficheiro.write(conteudo_html)
-ficheiro.close()
-
-json = """["""
-for i in range(len(nomes)):
-    json = json + f"""\n    {{"apelido":"{nomes[i][0]}", "nome":"{nomes[i][1]}"}}"""
-    if i < len(nomes)-1:
-        json = json + ","
-json = json + """\n]"""
-ficheiro = open("nomes.json", "w", encoding="utf-8")
-ficheiro.write(json)
 ficheiro.close()
