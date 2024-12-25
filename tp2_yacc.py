f5817c0156b5ddb1a49201d82b8fc3fe8a4f479d
Johnny <fonsecajanjao@gmail.com>
2024-12-25 17:49:02 +0000

    PARSER - em desenvolvimento

--- tp2_yacc.py
+++ tp2_yacc.py
@@ -1,3 +1,25 @@
 import ply.yacc as yacc
 import sys
-# from tp2_lex import tokens
+
+from tp2_lex import tokens
+
+''' Grammar
+Programa : Imports Funcs
+Imports : Import Imports
+        | Import
+Import : '#' INCLUDE '<' BIBLIO '>'
+Funcs : Func Funcs
+      | Func
+Func : TIPO ID '(' Inputs ')' '{' Comandos Output'}'
+Inputs : Input ',' Inputs
+       | Input
+Input : TIPO ID
+Comandos : Comando Comandos
+         | Comando
+Comando : ???????????
+Output : RETURN Ret
+Ret : ID 
+    | INT 
+    | FLOAT 
+    | STRING
+'''
